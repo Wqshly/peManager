81ee3979f0465b49aa2003385ee22b0d3c169d2c
chuan-1996 <491528074@qq.com>
2020-09-06 19:16:02 +0800

    生产环境跨域代理 Signed-off-by: chuan-1996 <491528074@qq.com>

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 import fi from "element-ui/src/locale/lang/fi";
 
 const qs = require('qs');
-
+const root = process.env.API_ROOT;
 const api = {
   async get(url) {
     try {
-      let res = await axios.get(url);
+      let res = await axios.get(root+url);
       res = res.data;
       return new Promise((resolve) => {
         resolve(res)
@@ -18,22 +18,7 @@ const api = {
   },
   async post(url, data) {
     try {
-      let res = await axios.post(url, qs.stringify(data));
-      res = res.data;
-      return new Promise((resolve, reject) => {
-        resolve(res)
-      })
-    } catch (err) {
-      alert('无法连接至服务器');
-      console.log('无法连接至服务器');
-    }
-  },
-  async post_JSON(url, data) {
-    try {
-      let headers = {
-        'Content-Type': 'application/json'
-      };
-      let res = await axios.post(url, data,headers);
+      let res = await axios.post(root+url, qs.stringify(data));
       res = res.data;
       return new Promise((resolve, reject) => {
         resolve(res)
@@ -50,7 +35,7 @@ const api = {
     form.append("time", (new Date()).getTime());
 
     try {
-      let res = await axios.post(url, form,{
+      let res = await axios.post(root+url, form,{
         'Content-Type': 'multipart/form-data',
       });
       res = res.data;
@@ -60,16 +45,6 @@ const api = {
     } catch (err) {
       console.log('无法连接至服务器');
     }
-  },
-  getQueryVariable(variable)
-  {
-    let query = window.location.search.substring(1);
-    let vars = query.split("&");
-    for (let i=0;i<vars.length;i++) {
-      let pair = vars[i].split("=");
-      if(pair[0] == variable){return pair[1];}
-    }
-    return(false);
   },
 
 };
